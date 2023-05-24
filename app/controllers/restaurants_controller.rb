797886031dc58f1984f8ab6b290899b038297fe9
Henrique Waxman <henriquewsouza@gmail.com>
2023-05-24 14:31:16 -0300

    restaurants crud done, missing reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,35 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[ show edit update destroy ]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
+    raise "Restaurant not found" unless @restaurant
+  end
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: %i[new index show] do
+  resources :restaurants, only: %i[new create index show] do
     resources :reviews, only: %i[new create]
   end
   resources :reviews, only: :destroy
